ced104bbb81421bfc688c0c992fc01630842de32
ta-data-lis <lisbon-ta-team@ironhack.com>
2021-04-07 16:39:50 +0100

    area and volume

--- Conflits.py
+++ Conflits.py
@@ -5,7 +5,7 @@ from math import pi, pow
 
 r = float(input ("Input the radius of the circle : "))
 
-#A = str(pi * r**2)
+A = str(pi * r**2)
 
 print ("The area of the circle with radius " + str(r) + " is: ")
 
@@ -13,7 +13,7 @@ print ("The area of the circle with radius " + str(r) + " is: ")
 # Volume of the Sphere
 
 
-#V = 4.0/3.0*pi* r**3
+V = 4.0/3.0*pi* r**3
 
 print("The volume of the sphere is: ")
 
