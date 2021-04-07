a4fd0456ab59f6113683b2d62a54e9b8681cc603
ta-data-lis <lisbon-ta-team@ironhack.com>
2021-04-07 16:17:12 +0100

    origin

--- Conflits.py
+++ Conflits.py
@@ -5,7 +5,7 @@ from math import pi, pow
 
 r = float(input ("Input the radius of the circle : "))
 
-A = str(pi * r**2)
+#A = str(pi * r**2)
 
 print ("The area of the circle with radius " + str(r) + " is: ")
 
