3a2be880e9ebc2b6a44af3657f58f4eb9b20b415
ta-data-lis <lisbon-ta-team@ironhack.com>
2021-04-07 15:01:07 +0100

    add new py file

---
+++ Conflits.py
@@ -0,0 +1,17 @@
+
+#  Area of the circle 
+
+from math import pi
+
+r = float(input ("Input the radius of the circle : "))
+
+#A = str(pi * r**2)
+
+print ("The area of the circle with radius " + str(r) + " is: ")
+
+
+# Volume of the Sphere
+
+#V = 4.0/3.0*pi* r**3
+
+print("The volume of the sphere is: ")10
