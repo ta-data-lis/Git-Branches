74c62af2c5cc26bff71cd2cb8db4d32e2da815e7
ta-data-lis <lisbon-ta-team@ironhack.com>
2021-10-09 13:09:50 +0100

    volume correction

--- Conflits.py
+++ Conflits.py
@@ -11,7 +11,7 @@ print ("The area of the circle with radius " + str(r) + " is: " + A)
 
 
 # Volume of the Sphere
-V = 4 * pi * r**2 
+V = 4/3 * pi * r**3
 
 
 print("The volume of the sphere is: ")
