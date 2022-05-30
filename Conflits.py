ba57180c86777c65f05c3e08ef2319537a06fdfd
ta-data-lis <data.ta.lis@ironhack.com>
2022-05-30 15:44:11 +0100

    new

--- Conflits.py
+++ Conflits.py
@@ -5,16 +5,15 @@ r = float(input ("Input the radius of the circle : "))
 
 
 #  Area of the circle 
-A = pi * pow(r,2)
+#A = 
 
 
-
-print ("The area of the circle with radius " + str(r) + " is: " + str(A))
+#print ("The area of the circle with radius " + str(r) + " is: " + str(A))
 
 
 # Volume of the Sphere
-V = 4/3 * pi * pow(r,3)
+#V = 
 
 
-print("The volume of the sphere is: ", V)
+#print("The volume of the sphere is: ", V)
 
