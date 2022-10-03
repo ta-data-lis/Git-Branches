6b4e6f1168fa4dc08f4f06f941282a922578d306
4kiUta <st.jose.alexandre@gmail.com>
2022-10-03 17:47:08 +0100

    area solved

--- Conflits.py
+++ Conflits.py
@@ -4,9 +4,9 @@ from math import pi, pow
 r = float(input ("Input the radius of the circle : "))
 
 #  Area of the circle 
-# A = 
+A = 2 * pi * r**2
 
-#print ("The area of the circle with radius " + str(r) + " is: " + str(A))
+print ("The area of the circle with radius " + str(r) + " is: " + str(A))
 
 
 # Volume of the Sphere
