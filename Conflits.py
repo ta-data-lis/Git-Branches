cd85e34a19a6024f72503558a6512353d6c6d50f
4kiUta <st.jose.alexandre@gmail.com>
2022-10-04 08:44:18 +0100

    reset

--- Conflits.py
+++ Conflits.py
@@ -4,18 +4,13 @@ from math import pi, pow
 r = float(input ("Input the radius of the circle : "))
 
 #  Area of the circle 
-<<<<<<< HEAD
-A = pi * pow(r, 2)
-=======
 # A = 
->>>>>>> c3fa248b9a8501d75ccc4a17cf0be9f3a6097138
 
 #print ("The area of the circle with radius " + str(r) + " is: " + str(A))
 
 
 # Volume of the Sphere
 # V = 
-
 
 
 # print("The volume of the sphere is: ", V)
